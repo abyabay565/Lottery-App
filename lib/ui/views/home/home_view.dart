27e5a8157d11261d6e12466650a08e01d638a513
abinbenny <abin@webcastle.in>
2024-10-03 22:27:16 +0530

    test

--- lib/ui/views/home/home_view.dart
+++ lib/ui/views/home/home_view.dart
@@ -8,6 +8,7 @@ import 'home_viewmodel.dart';
 class HomeView extends StackedView<HomeViewModel> {
   const HomeView({Key? key}) : super(key: key);
 
+
   @override
   Widget builder(
     BuildContext context,
